a13d4e65c5e39c2414a2e632f1b7079133e8cc31
“carlos <cordovamanuel22@icloud.com>
2022-11-23 14:00:56 -0800

    GET API working

--- api/api.js
+++ api/api.js
@@ -1,19 +1,35 @@
-   async function getHomeList(){
-  
-   fetch(
-     "https://realty-in-us.p.rapidapi.com/properties/list-for-sale?state_code=ca&city= +"userInput"+&offset=0&limit=10",
-     {
-       method: "GET",
-       headers: {
-         "X-RapidAPI-Key": "d4fc518e26msh30c554e610cfda0p11e0c1jsn3fbb7b26b0c5",
-         "X-RapidAPI-Host": "realty-in-us.p.rapidapi.com",
-       },
-     }
-   )
-     .then((response) => response.json())
-     .then((response) => console.log(response))
-     .catch((err) => console.error(err));
- };
+async function getListing(){
 
-export default getHomeList ;
 
+
+const options = {
+  method: "GET",
+  url: "https://realty-in-us.p.rapidapi.com/properties/v2/list-for-sale",
+  params: {
+    city: "New York City",
+    state_code: "NY",
+    offset: "0",
+    limit: "10",
+    sort: "relevance",
+  },
+  headers: {
+    "X-RapidAPI-Key": "d4fc518e26msh30c554e610cfda0p11e0c1jsn3fbb7b26b0c5",
+    "X-RapidAPI-Host": "realty-in-us.p.rapidapi.com",
+  },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+
+
+};
+
+ getListing();
+
+ export default getListing;
--- src/app.js
+++ src/app.js
@@ -1,28 +1,37 @@
+//! modules 
+//import getListing() from "../api/api"
 //! GLOBAL
-const userInputUI = document.querySelector("#user-input").value; //? input from html
+const userInputUI = document.querySelector("#user-input"); //? input from html
 const searchUI = document.querySelector("#search"); // ? button from html
 const formUI = document.querySelector("#form");
 const printContainerUI = document.querySelector("#print-container");
 
 //! event listener
 
-searchUI.addEventListener("click", getHomeList());
+searchUI.addEventListener("click", async function getHomeList() {
+  const options = await {
+    method: "GET",
+    url: "https://realty-in-us.p.rapidapi.com/properties/v2/list-for-sale",
+    params: {
+      city: `${userInputUI.value}`,
+      state_code: "ca",
+      offset: "0",
+      limit: "10",
+      sort: "relevance",
+    },
+    headers: {
+      "X-RapidAPI-Key": "d4fc518e26msh30c554e610cfda0p11e0c1jsn3fbb7b26b0c5",
+      "X-RapidAPI-Host": "realty-in-us.p.rapidapi.com",
+    },
+  };
 
-async function getHomeList() {
-  let userInput = userInputUI.value;
-  fetch(
-    "https://realty-in-us.p.rapidapi.com/properties/list-for-sale?state_code=ca&city= " +
-      userInput +
-      "&offset=0&limit=10",
-    {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": "d4fc518e26msh30c554e610cfda0p11e0c1jsn3fbb7b26b0c5",
-        "X-RapidAPI-Host": "realty-in-us.p.rapidapi.com",
-      },
-    }
-  )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-}
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
+
