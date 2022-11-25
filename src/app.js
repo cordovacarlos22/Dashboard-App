e3d0f05cae43291f8e9c1757718874709f0544d3
“carlos <cordovamanuel22@icloud.com>
2022-11-25 11:48:58 -0800

    html update

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 //! modules 
-
+import { printResult} from "./functions.js";
+import {validateInput} from "./functions.js"
 //! GLOBAL
 const userInputUI = document.querySelector("#user-input-city"); //? input from UI to get city
 const userInputStateUI = document.querySelector("#user-input-state"); //? input from UI to get State
 const sortBy = document.querySelector("#sort-by");//? input form UI to sort-
 const minMaxBeds = document.querySelector("#beds");
+const minMaxBaths = document.querySelector("#baths");
 const searchUI = document.querySelector("#search"); // ? button from html
-const formUI = document.querySelector("#form");
-const printContainerUI = document.querySelector("#print-container");
+const formUI = document.querySelector('#form')//? UI FORM 
+
+
+
 
 //! event listener
 
 searchUI.addEventListener("click", async function getHomeList() {
+  validateInput();
+
   const options = await {
     method: "GET",
     url: "https://realty-in-us.p.rapidapi.com/properties/v2/list-for-sale",
@@ -21,7 +27,8 @@ searchUI.addEventListener("click", async function getHomeList() {
       offset: "0",
       limit: "9",
       sort: "relevance",
-      beds_min: `${minMaxBeds.value}`,
+      beds_max: `${minMaxBeds.value}`,
+      baths_max: `${minMaxBaths.value}`,
     },
     headers: {
       "X-RapidAPI-Key": "d4fc518e26msh30c554e610cfda0p11e0c1jsn3fbb7b26b0c5",
@@ -29,7 +36,7 @@ searchUI.addEventListener("click", async function getHomeList() {
     },
   };
 
-  axios
+ axios 
     .request(options)
     .then(function (response) {
      console.log(response.data.properties);
@@ -43,25 +50,6 @@ searchUI.addEventListener("click", async function getHomeList() {
 });
 
 
-
-
-const printResult = (datos) => {
-  datos.forEach((data) => {
-    // console.log(data.thumbnail); //? test data to make sure I am getting to the right path
-    printContainerUI.innerHTML += `
-    <div class="card">
-      <img class="card-thumbnail " src="${data.thumbnail}" / >
-      <p class="text-primary">$${data.price} ${data.prop_type}</p>
-      <p>${data.beds} beds - ${data.baths} baths</p>
-      <span class="text-black-50">${data.address.line}, ${data.address.city}, ${data.address.postal_code}, ${data.address.state_code} </span>
-      <a href="${data.rdc_web_url}" target="_blank">Details</a>
-    </div>
-    
-    
-    `;
-    
-  });
-};
 
 
 
---
+++ src/functions.js
@@ -0,0 +1,76 @@
+//! global 
+const userInputUI = document.querySelector("#user-input-city"); //? input from UI to get city
+const userInputStateUI = document.querySelector("#user-input-state"); //? input from UI to get State
+const sortBy = document.querySelector("#sort-by");//? input form UI to sort-
+const minMaxBeds = document.querySelector("#beds");
+const formUI = document.querySelector('#form')//? UI FORM 
+const printContainerUI = document.querySelector("#print-container");
+const minMaxBaths = document.querySelector("#baths");
+const searchUI = document.querySelector("#search"); // ? button from html
+const fillCompletyUI = document.querySelector("#fillComplety-btn");
+
+//! FUNCTIONS 
+//? function to print UI 
+export const printResult = (datos) => {
+ 
+  datos.forEach((data) => {
+    // console.log(data.thumbnail); //? test data to make sure I am getting to the right path
+    printContainerUI.innerHTML += `
+    <div class="card">
+      <img class="card-thumbnail " src="${data.thumbnail}" / >
+      <p class="text-primary">$${data.price} ${data.prop_type}</p>
+      <p>${data.beds} beds - ${data.baths} baths</p>
+      <span class="text-black-50 fs-6">${data.address.line}, ${data.address.city}, ${data.address.postal_code}, ${data.address.state_code} </span>
+      <a class="text-decoration-none" href="${data.rdc_web_url}" target="_blank">Details</a>
+    </div>
+    
+    
+    `;
+      // clear input form  after an input 
+  userInputUI.value = '';
+  userInputStateUI.value = '';
+  minMaxBaths.value = '';
+  minMaxBeds.value = '';
+  });
+};
+
+//? FUNCTION TO VALIDATE INPUTS 
+
+export const validateInput = () =>{
+    // does not let send form if not fill complete
+
+  if (
+    (userInputUI.value === "") |
+    (userInputStateUI.value === "") |
+    (minMaxBeds.value === "") |
+    (minMaxBaths.value === "")
+  ) {
+
+  
+   searchUI.classList.add("d-none");
+   // to show update button.
+   fillCompletyUI.classList.remove("d-none");
+   
+   
+
+   wait()
+  
+    return;
+  }
+
+}
+//!  arrow function to wait 3 secons to execute showsearchUI function 
+
+const wait = () => {
+  let timeout;
+  timeout = setTimeout(showsearchUI, 1000);
+};
+
+export const showsearchUI = () =>{
+  
+  searchUI.classList.remove("d-none");
+   // to show update button.
+   fillCompletyUI.classList.add("d-none");
+   
+
+}
